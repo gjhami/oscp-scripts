3d20992678ccc79d384ba5017191d35a42e9ba65
unknown <georgejosephhamilton@gmail.com>
2024-01-22 22:58:47 -0500

    Quieter
    Make install commands quieter and fix path to shared folder mount.

--- initial-setup.sh
+++ initial-setup.sh
@@ -3,7 +3,7 @@
 # Installs useful programs and mounts a shared folder
 
 # Update the sysetem and install some commonly used tools
-apt update && apt upgrade --assume-yes --quiet
+apt update --quiet && apt upgrade --assume-yes --quiet
 apt install neo4j --assume-yes --quiet
 apt install peass --assume-yes --quiet
 apt install libreoffice --assume-yes --quiet
@@ -14,13 +14,13 @@ python -m pip install bloodhound
 
 # Install vscode
 cd /home/kali/Downloads/
-wget 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' -O vscode.deb
+wget 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64' -O vscode.deb --quiet
 apt install ./vscode.deb --assume-yes --quiet
 
 # Setup, mount, and symlink the shared folder
 mkdir /mnt/hgfs
 echo 'vmhgfs-fuse /mnt/hgfs fuse defaults,allow_other,nofail 0 0' >> /etc/fstab
-ln -s /etc/hgfs/Exclusions /home/kali/Desktop/shared
+ln -s /mnt/hgfs/Exclusions /home/kali/Desktop/shared
 
 # Modify proxychains TCP timeouts to optimize for nmap scanning
 sudo sed -i 's/tcp_read_time_out.*/tcp_read_time_out 150/g' /etc/proxychains4.conf
